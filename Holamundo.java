e2c651101fa8323a6502cc63c97735ad8a608eb2
Devjean2000 <jeanpaulbuelvas36@gmail.com>
2023-08-14 11:02:44 -0500

    Trabajando con Java

---
+++ Holamundo.java
@@ -0,0 +1,11 @@
+public class Holamundo {
+    public static void main(String args[]){
+        System.out.println("¡Hola, mundo!");
+
+        int num = 3;
+
+        do {
+            System.out.println(num++);
+        } while (num <= 3);
+    }
+}
